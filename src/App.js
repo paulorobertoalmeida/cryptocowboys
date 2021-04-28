9d090b14b2f7cb08b914f87a5119274d88b71762
Paulo Almeida <paulo.roberto.a@hotmail.com>
2021-04-28 19:27:30 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navtop from './components/Navtop'
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-content">
+    <Navtop />
+    <h1>Hello</h1>
+
+    </div>
+  );
+}
+
+export default App;
